6b85f385f5f678454b1863ad5b2508595b86a618
xloc <xloc.cc@outlook.com>
2023-04-11 01:42:38 -0700

    add extendToHigherOctaves

--- src/ChordButton.tsx
+++ src/ChordButton.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
 import classNames from 'classnames';
+import React from 'react';
 
+import { Chord, Note, note } from "tonal";
 import * as Tone from 'tone';
-import { Chord } from "tonal";
 
 
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 
 
+const extendToHigherOctaves = (notes: string[], rootOctave: number | undefined) => {
+  const chroma = notes.map(Note.chroma) as number[];
+  let octave = rootOctave ?? 4;
+  return notes.map((note, i) => {
+    if (i > 0 && chroma[i] < chroma[i - 1]) {
+      octave += 1;
+    }
+    return `${note}${octave}`;
+  });
+};
+
+const wrapAround = (notes: string[], octave: number | undefined) => {
+  return notes.map((n) => `${n}${octave ?? 4}`);
+};
+
+
 interface ChordButtonProps {
   text: string;
 }
-
 export default function ChordButton(props: ChordButtonProps) {
   const { text } = props;
 
-  const notes = Chord.get(text).notes;
-  const playNotes = notes.map(n => `${n}4`);
+  const chord = Chord.get(text);
+  const notes = chord.notes;
+  // const playNotes = extendToHigherOctaves(notes, 4);
+  const playNotes = wrapAround(notes, 4);
 
 
   const isTouchScreen = 'ontouchstart' in window ? true : undefined;
@@ -24,6 +41,7 @@ export default function ChordButton(props: ChordButtonProps) {
   const genOnClick = (text: string) => {
     return (e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>) => {
       console.log(playNotes);
+      // setTimeout(() => { synth.releaseAll(); }, 5 * 1000); // guard
       synth.triggerAttackRelease(playNotes, "8n");
     };
   };
